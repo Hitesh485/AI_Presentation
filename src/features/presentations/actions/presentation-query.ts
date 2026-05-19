769ab9a527d6e74a71a8b4eec22903c85a32db40
Hitesh485 <hitesh.kumar2309@gmail.com>
2026-05-20 01:15:23 +0530

    First commit

---
+++ src/features/presentations/actions/presentation-query.ts
@@ -0,0 +1,36 @@
+import { createServerFn } from "@tanstack/react-start";
+import { presentationIdInputSchema } from "../types/schemas";
+import { authMiddleware } from "#/middleware/auth";
+import { prisma } from "#/lib/db";
+
+
+export const getPresentationWithSlides = createServerFn({method: "GET"})
+.inputValidator((data:unknown)=>presentationIdInputSchema.parse(data))
+.middleware([authMiddleware])
+.handler(async({data, context}) => {
+    const userId = context?.session?.user?.id;
+
+    const row = await prisma.presentation.findFirst({
+        where: {
+            id: data.id,
+            userId,
+        },
+        include: {
+            slides: {
+                orderBy: {order: "asc"},
+            },
+        },  
+    })
+    return row;
+})
+
+export const listPresentation = createServerFn({method:"GET"})
+.middleware([authMiddleware])
+.handler(async({context}) => {
+    const userID = context?.session?.user?.id;
+
+    return prisma.presentation.findMany({
+        where:{userId},
+        orderBy:{updatedAt:"desc"}
+    })
+})
